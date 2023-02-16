cc2adb438075a27808a4dade487bb1051ba50f17
Jordon Marchesano <102629027+cHoMpDiSs@users.noreply.github.com>
2023-02-15 22:28:12 -0800

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,24 @@
-def swap(my_list,index1,index2):
-    temp = my_list[index1]
-    my_list[index1] = my_list[index2]
-    my_list[index2] = temp
-
-def pivot(my_list, pivot_index, end_index):
-    swap
+#divide
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quickSort(arr,low,high):
+   if low < high:
+      # index
+      pi = partition(arr,low,high)
+      # sort the partitions
+      quickSort(arr, low, pi-1)
+      quickSort(arr, pi+1, high)
+# main
+arr = [2,5,3,8,6,5,4,7]
+n = len(arr)
+quickSort(arr,0,n-1)
